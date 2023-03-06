740d8ec781d054a1c983c12fd8459e573a3d94c3
jason <jason0kenyon@gmail.com>
2023-03-05 19:07:08 -0500

    lots of stuff added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -136,7 +136,7 @@
 
 (set-face-attribute 'default nil :font jas/default-fixed-font :height jas/default-font-size )
 (set-face-attribute 'fixed-pitch nil :font jas/default-fixed-font :height jas/default-font-size )
-(set-face-attribute 'variable-pitch nil :font jas/default-fixed-font :height jas/default-font-size )
+(set-face-attribute 'variable-pitch nil :font jas/default-variable-font :height jas/default-font-size )
 (use-package diminish)
 (use-package all-the-icons)
 (use-package doom-themes
@@ -161,7 +161,10 @@
   (diminish 'buffer-face-mode))
 (with-eval-after-load 'org-indent
   (diminish 'org-indent-mode))
+(with-eval-after-load 'face-remap
+  (diminish 'text-scale-mode))
 
+(use-package counsel)
 (recentf-mode 1)
   (use-package savehist
     :init
@@ -192,45 +195,49 @@
       (completion-category-overrides '((file (styles basic partial-completion)))))
 (use-package consult-flycheck)
 
-(setq org-src-tab-acts-natively t)
-  (setq org-src-fontify-natively t)
-  (require 'org-tempo)
-  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-  (add-hook 'org-mode-hook (lambda() (display-line-numbers-mode 0)))
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-  (add-hook 'org-mode-hook 'org-indent-mode)
-  (add-hook 'org-mode-hook 'visual-line-mode)
-  (add-hook 'org-mode-hook (lambda () (set-fringe-mode 10)))
-  (use-package org-bullets)
+(use-package org-superstar)
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+      (setq org-src-tab-acts-natively t)
+      (setq org-src-fontify-natively t)
+      (require 'org-tempo)
+      (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+      (add-hook 'org-mode-hook (lambda() (display-line-numbers-mode 0)))
+      (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+      (add-hook 'org-mode-hook 'org-indent-mode)
+      (add-hook 'org-mode-hook 'visual-line-mode)
+      (add-hook 'org-mode-hook (lambda () (set-fringe-mode 10)))
   (setq org-ellipsis " ▼"
-        org-superstar-remove-leading-stars t
-        org-hide-emphasis-markers t)
-(add-hook 'org-mode-hook 'variable-pitch-mode)
-(add-hook 'org-mode-hook 'visual-line-mode)
-(set-face-attribute 'org-document-title nil :font jas/default-fixed-font :weight 'bold :height 1.3)
-(dolist (face '((org-level-1 . 2.0)
-                (org-level-2 . 2.0)
-                (org-level-3 . 1.5)
-                (org-level-4 . 1.2)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1))) (set-face-attribute (car face) nil :font jas/default-variable-font :weight 'medium :height (cdr face)))
+          org-superstar-remove-leading-stars t
+          org-hide-emphasis-markers t
+          org-superstar-headline-bullets-list '("◉" "●" "○" "◆" "●" "○" "◆")
+          org-superstar-item-bullet-alist '((?+ . ?◆) (?- . ?•))
+          org-superstar-special-todo-items 'hide)
+    (add-hook 'org-mode-hook 'variable-pitch-mode)
+    (add-hook 'org-mode-hook 'visual-line-mode)
+    (set-face-attribute 'org-document-title nil :font jas/default-fixed-font :weight 'bold :height 1.3)
+    (dolist (face '((org-level-1 . 2.0)
+                    (org-level-2 . 2.0)
+                    (org-level-3 . 1.5)
+                    (org-level-4 . 1.2)
+                    (org-level-5 . 1.1)
+                    (org-level-6 . 1.1)
+                    (org-level-7 . 1.1)
+                    (org-level-8 . 1.1))) (set-face-attribute (car face) nil :font jas/default-variable-font :weight 'medium :height (cdr face)))
 
-(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-(set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-(set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+    (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+    (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+    (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+    (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)
 
-(setq org-directory "~/projects/notes"
-      org-agenda-files '("~/projects/notes" "~/projects/notes/daily"))
+(setq org-directory "~/Dropbox/notes"
+      org-agenda-files '("~/Dropbox/notes" "~/Dropbox/notes/daily"))
 (setq org-agenda-window-setup 'only-window)
 (use-package org-fancy-priorities)
 (setq org-fancy-priorities-list '("⚡" "⚠" "❗"))
@@ -245,6 +252,16 @@
     "CANCELLED(c)"
     )))
 (setq org-agenda-remove-tags t)
+
+(setq org-clock-mode-line-entry nil)
+  (use-package org-pomodoro
+    :after org)
+(setq org-pomodoro-length 45)
+(setq org-pomodoro-short-break-length 25)
+(setq org-pomodoro-long-break-length 45)
+    (jas/leader-def
+ :states 'normal
+      "op"  'org-pomodoro)
 
 (require 'evil-org-agenda)
 (evil-org-agenda-set-keys)
@@ -261,7 +278,8 @@
 )
 
 (use-package denote)
-(setq denote-directory "~/projects/notes")
+(setq denote-directory "~/Dropbox/notes")
+
 (setq denote-templates '((daily . "* Journal\n\n* Tasks\n** TODO [/]\n1. [ ] Mindfulness(10min)\n2. [ ] Journaling(5min)\n3. [ ] Check Out\n** Notes") (math-landing-page . "* meta-analysis\n* Source")))
 
 (defun daily-journal ()
@@ -277,14 +295,14 @@
 
 (use-package citar
   :custom
-  (citar-bibliography '("~/projects/notes/shared/latex/templates/refs.bib")))
+  (citar-bibliography '("~/Dropbox/shared-notes/latex/templates/refs.bib")))
 (use-package citar-denote
   :diminish
   :after citar denote
   :config
   (citar-denote-mode)
   (setq citar-open-always-create-notes t))
-(setq citar-library-paths '("~/library/papers/" "~/projects/notes/shared/bookshelf"))
+(setq citar-library-paths '("~/library/papers/" "~/Dropbox/shared-notes/bookshelf/papers"))
 (setq citar-templates
       '((main . "${author editor:30}     ${date year issued:4}     ${title:48}")
         (suffix . "          ${=key= id:15}    ${=type=:12}    ${tags keywords:*}")
@@ -359,7 +377,128 @@
 "cg" 'consult-ripgrep
 )
 
+(server-start)
+  (use-package exwm)
+  (defun jas/bind-command (key command &rest bindings)
+  "Bind KEYs to COMMANDs globally"
+  (while key
+    (exwm-input-set-key (kbd key)
+                        `(lambda ()
+                           (interactive)
+                           (jas/run-in-background ,command)))
+    (setq key (pop bindings)
+          command (pop bindings))))
+  (defun make-external-command (command)
+    (lambda ()
+      (interactive)
+      (let ((buffer-name (car (split-string command))))
+        (cond
+         ((equal buffer-name (buffer-name))
+          (switch-to-last-used-buffer))
+         ((get-buffer buffer-name)
+          (switch-to-buffer (get-buffer buffer-name)))
+         (t (start-process-shell-command buffer-name nil command))))))
+
+  (setq exwm-input-prefix-keys
+        '(?\M-x))
+
+  (defun jas/exwm-update-class ()
+    (exwm-workspace-rename-buffer exwm-class-name))
+  (add-hook 'exwm-update-class-hook #'jas/exwm-update-class)
+  (defun jas/run-in-background (command)
+    (let ((command-parts (split-string command "[ ]+")))
+      (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
+
+(setq exwm-workspace-number 5)
+ (setq exwm-input-global-keys
+        `(
+          ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+          ([?\s-r] . exwm-reset)
+
+          ;; Move between windows
+          ([?\s-h] . windmove-left)
+          ([?\s-l] . windmove-right)
+          ([?\s-k] . windmove-up)
+          ([?\s-j] . windmove-down)
+          ([?\s-q] . exwm-workspace-delete)
+          ([?\s-s] . evil-window-vsplit)
+          ([?\s-v] . evil-window-split)
+          ([?\s-p] . exwm-workspace-switch)
+          ([?\s-w] . evil-window-delete)
+
+
+          ;; Switch workspace
+          ;;          ([?\s-w] . exwm-workspace-switch)
+
+          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))))
+
+(jas/bind-command
+"<XF86AudioMute>" "amixer set Master toggle"
+"<XF86AudioLowerVolume>" "amixer set Master 10%-"
+"<XF86AudioRaiseVolume>" "amixer set Master 10%+"
+"<XF86MonBrightnessUp>" "brightnessctl set 10%+"
+"<XF86MonBrightnessDown>" "brightnessctl set 10%-"
+"s-b" "qutebrowser")
+(exwm-input-set-key (kbd "s-x") 'counsel-linux-app)
+
+(defvar jas/polybar-process nil
+  "Holds the process of the running Polybar instance, if any")
+
+(defun jas/kill-panel ()
+  (interactive)
+  (when jas/polybar-process
+    (ignore-errors
+      (kill-process jas/polybar-process)))
+  (setq jas/polybar-process nil))
+
+(defun jas/start-panel ()
+  (interactive)
+  (jas/kill-panel)
+  (setq jas/polybar-process (start-process-shell-command "polybar" nil "polybar panel")))
+
+(defun jas/polybar-exwm-workspace ()
+  (pcase exwm-workspace-current-index
+    (0 "")
+    (1 "")
+    (2 "")
+    (3 "")
+    (4 "")))
+(defun jas/send-polybar-hook (module-name hook-index)
+  (start-process-shell-command "polybar-msg" nil (format "polybar-msg hook %s %s" module-name hook-index)))
+
+(defun jas/send-polybar-exwm-workspace ()
+  (jas/send-polybar-hook "exwm-workspace" 1))
+
+;; Update panel indicator when workspace changes
+(add-hook 'exwm-workspace-switch-hook #'jas/send-polybar-exwm-workspace)
+
+(require 'exwm-randr)
+(exwm-randr-enable)
+(start-process-shell-command "xrandr" nil "xrandr --output eDP-1 --primary --mode 3456x2160 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off")
+(defun jas/exwm-init-hook ()
+  (jas/run-in-background "dropbox")
+  (jas/run-in-background "nm-applet")
+  (jas/run-in-background "blueman-applet")
+  (jas/run-in-background "pasystray")
+  ;; Make workspace 1 be the one where we land at startup
+  (exwm-workspace-switch-create 1)
+ ;; Start the Polybar panel
+  (jas/start-panel)
+  )
+;; When EXWM starts up, do some extra confifuration
+(add-hook 'exwm-init-hook #'jas/exwm-init-hook)
+(exwm-enable)
+
 (use-package vterm)
   (add-hook 'vterm-mode-hook (lambda() (display-line-numbers-mode 0)))
   (add-hook 'eshell-mode-hook (lambda() (display-line-numbers-mode 0)))
 (setq delete-by-moving-to-trash t)
+(jas/leader-def
+:states 'normal
+"t" 'vterm)
